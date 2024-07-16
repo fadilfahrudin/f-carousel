bdb864bc30f154ef862a49480f800a9de4b6503b
fadilfahrudin <fadilfahrudin32@gmail.com>
2024-07-16 07:38:02 +0700

    update 3d slide carousel

--- src/modules/sandbox2/main.js
+++ src/modules/sandbox2/main.js
@@ -13,16 +13,16 @@ function moveToSelected(element) {
     var prevSecond = $(prev).prev();
     var nextSecond = $(next).next();
 
-    $(selected).removeClass().addClass("selected");
+    $(selected).removeClass().addClass("slide selected");
 
-    $(prev).removeClass().addClass("prev");
-    $(next).removeClass().addClass("next");
+    $(prev).removeClass().addClass("slide prev");
+    $(next).removeClass().addClass("slide next");
 
-    $(nextSecond).removeClass().addClass("nextRightSecond");
-    $(prevSecond).removeClass().addClass("prevLeftSecond");
+    $(nextSecond).removeClass().addClass("slide nextRightSecond");
+    $(prevSecond).removeClass().addClass("slide prevLeftSecond");
 
-    $(nextSecond).nextAll().removeClass().addClass('hideRight');
-    $(prevSecond).prevAll().removeClass().addClass('hideLeft');
+    $(nextSecond).nextAll().removeClass().addClass('slide hideRight');
+    $(prevSecond).prevAll().removeClass().addClass('slide hideLeft');
 
 }
 
@@ -43,35 +43,65 @@ $(document).keydown(function (e) {
 });
 
 
-$('#carousel div').click(function () {
-    moveToSelected($(this));
-});
 
-$('#prev').click(function () {
-    moveToSelected('prev');
-});
-
-$('#next').click(function () {
-    moveToSelected('next');
-});
 
 
 const initSlide = () => {
-    $('.slide').not(".hideLeft, .hideRight, .prev, .next, .prevLeftSecond, .nextRightSecond, .selected").addClass("hideRight")
-
-    $(".slide").eq(0).addClass("hideLeft")
-    $(".slide").eq(1).addClass("prevLeftSecond")
-    $(".slide").eq(2).addClass("prev")
-    $(".slide").eq(3).addClass("selected")
-    $(".slide").eq(4).addClass("next")
-    $(".slide").eq(5).addClass("nextRightSecond")
-    $(".slide").eq(6).addClass("hideRight")
+    $(".slide").eq(3).addClass("prevLeftSecond")
+    $(".slide").eq(4).addClass("prev")
+    $(".slide").eq(5).addClass("selected")
+    $(".slide").eq(6).addClass("next")
+    $(".slide").eq(7).addClass("nextRightSecond")
 
 }
 
+const slide = $(".slide")
+
 $(document).ready(function () {
-    $(".slide").each(function (i, element) {
-        $('#carousel').append($(element).clone());
-    })
+    $('#carousel').append($(slide).eq(0).clone().addClass("hideRight"));
+    $('#carousel').append($(slide).eq(1).clone().addClass("hideRight"));
+    $('#carousel').append($(slide).eq(2).clone().addClass("hideRight"));
+    $('#carousel').prepend($(slide).eq(slide.length - 1).clone().addClass("hideLeft"));
+    $('#carousel').prepend($(slide).eq(slide.length - 2).clone().addClass("hideLeft"));
+    $('#carousel').prepend($(slide).eq(slide.length - 3).clone().addClass("hideLeft"));
     initSlide()
+
+    $('.slide').each((i, el) => {
+        if (!$(el).hasClass("nextRightSecond") && !$(el).hasClass("prevLeftSecond") &&  !$(el).hasClass("prev") &&  !$(el).hasClass("next") &&  !$(el).hasClass("hideLeft") &&  !$(el).hasClass("hideRight") &&  !$(el).hasClass("selected")) {
+            $(el).addClass("hideRight")
+        }
+    })
+
+
+
+    $('#carousel .slide').click(function () {
+        moveToSelected($(this));
+
+    });
+
+    $('#prev').click(function () {
+        moveToSelected('prev');
+        if ($('.slide').eq(0).hasClass('prevLeftSecond')) {
+            setTimeout(() => {
+                $('.slide').css('transition', 'none')
+                $('.slide img').css('transition', 'none')
+                moveToSelected($('.slide').eq($(".slide").length - 4));
+            }, 500)
+        }
+        $('.slide').css('transition', 'transform 1s, left 1s, opacity 1s, z-index 0s')
+        $('.slide img').css('transition', 'width 1s')
+    });
+
+    $('#next').click(function () {
+        moveToSelected('next');
+        if ($('.slide').eq($(".slide").length - 1).hasClass('nextRightSecond')) {
+            setTimeout(() => {
+                $('.slide').css('transition', 'none')
+                $('.slide img').css('transition', 'none')
+                moveToSelected($('.slide').eq(3));
+            }, 500)
+        }
+        $('.slide').css('transition', 'transform 1s, left 1s, opacity 1s, z-index 0s')
+        $('.slide img').css('transition', 'width 1s')
+    });
 })
